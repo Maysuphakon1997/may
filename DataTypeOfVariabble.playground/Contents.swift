680872801d00299602699f75d857886706397bec
may <student23@Student23.local>
2019-03-18 15:03:52 +0700

    Start DataType

---
+++ DataTypeOfVariabble.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon" //นี่คือการประกาศ ที่อาศัยชนิดของ Value บอก Datatype
+var mySurname: String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+//การแสดงผลบน Console
+print("This is Console")
+print("myName = \(myName)")
+
+
+//lnteger Type หรือ จำนวนที่ไม่มีเศษ
+var number1 = 100
+var number2: Int = 200
+
+//Double Type หรือ จำนวนที่มีเศษ
+var number3 = 3.1416
+var number4:Double = 10.234
+
+//Boolean Type คือ จำนวนที่มีแค่ค่า true, false
+var status = true
+var status2: Bool = false
